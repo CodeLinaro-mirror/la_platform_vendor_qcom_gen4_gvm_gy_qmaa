e759c1532944808492ae1db3f378e12b68eb7c37
Arun Raj Shanmugam <arunshan@qti.qualcomm.com>
2026-04-08 19:54:03 +0530

    Compilation fix for VENDOR.17.0.1
    TARGET_KERNEL_DLKM_DISABLE := true
    TARGET_KERNEL_DLKM_WLAN_OVERRIDE := true
    TARGET_KERNEL_DLKM_ESE_OVERRIDE := false
    TARGET_DISABLE_DATA_ETH_DLKM := true (new flag to disable data-eth)
    
    Change-Id: Ic2f5bd3fa0e8fd4b9dc47b3d250525a1b8db76d4
    Signed-off-by: Arun Raj Shanmugam <arunshan@qti.qualcomm.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
 include device/qcom/gen4_gvm_gy/BoardConfig.mk
-
+TARGET_KERNEL_DLKM_DISABLE := true
 -include vendor/qcom/defs/board-defs/system/*.mk
 -include vendor/qcom/defs/board-defs/vendor/*.mk
